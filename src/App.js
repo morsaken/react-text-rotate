bc298d680ca2e6398450dc9f2ef9fe71357c6e61
Orhan POLAT <orhanp@gmail.com>
2021-10-29 23:57:24 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import { useCallback, useEffect, useRef, useState } from 'react';
+import PropTypes from 'prop-types';
 
-function App() {
+let timer;
+
+const App = ({ texts, period = 2000, speed = 1 }) => {
+  const mounted = useRef(false);
+  const [text, setText] = useState('');
+  const textRef = useRef(text);
+  textRef.current = text;
+  const [index, setIndex] = useState(0);
+  const indexRef = useRef(index);
+  indexRef.current = index;
+  const [deleting, setDeleting] = useState(false);
+  const deletingRef = useRef(deleting);
+  deletingRef.current = deleting;
+
+  useEffect(() => {
+    mounted.current = true;
+
+    if (Array.isArray(texts)) {
+      tick();
+    }
+
+    return () => {
+      mounted.current = false;
+
+      if (timer) clearTimeout(timer);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (Array.isArray(texts) && index >= texts.length) {
+      setIndex(0);
+    }
+  }, [index]);
+
+  const tick = useCallback(() => {
+    if (mounted.current) {
+      const deleting = deletingRef.current;
+      const index = indexRef.current;
+      const fullTxt = texts[index];
+
+      let text = textRef.current;
+
+      if (deleting) {
+        text = fullTxt.substring(0, text.length - 1);
+      } else {
+        text = fullTxt.substring(0, text.length + 1);
+      }
+
+      let delta = 200 - Math.random() * 70;
+
+      if (deleting) delta /= 2;
+
+      delta /= speed;
+
+      if (!deleting && text === fullTxt) {
+        delta = period;
+        setDeleting(true);
+      } else if (deleting && text === '') {
+        setDeleting(false);
+        setIndex(state => state + 1);
+        delta = 500;
+
+      }
+
+      setText(text);
+
+      timer = setTimeout(tick, delta);
+    }
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <span>{text}</span>
   );
-}
+};
+
+App.propTypes = {
+  texts: PropTypes.arrayOf(PropTypes.string).isRequired,
+  period: PropTypes.number,
+  speed: PropTypes.number
+};
 
 export default App;
